ab17b7033a407a2d692b7d689987e3298b7c1f7a
Robert_Howell <109364552+Robert-Howell@users.noreply.github.com>
2026-04-27 17:43:18 -0400

    Update data_quality.py

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -8,15 +8,37 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 data_quality_checks = [],
+                 redshift_conn_id = "",
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.data_quality_checks = data_quality_checks
+        self.redshift_conn_id = redshift_conn_id
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        self.log.info('Connection Redshift')
+        redshift = PostgresHook(self.redshift_conn_id)
+        cnt_err  = 0
+        
+        for check_step in self.data_quality_checks:
+            
+            check_query     = check_step.get('data_check_sql')
+            expected_result = check_step.get('expected_value')
+            
+            result = redshift.get_records(check_query)[0]
+            
+            self.log.info(f"Running query   : {check_query}")
+            self.log.info(f"Expected result : {expected_result}")
+            self.log.info(f"Check result    : {result}")
+            
+            
+            if result[0] != expected_result:
+                cnt_err += 1
+                self.log.info(f"Data quality check fails At   : {check_query}")
+                
+            
+        if cnt_err > 0:
+            self.log.info('Data quality checks - Failed !')
+        else:
+            self.log.info('Data quality checks - Passed !')
